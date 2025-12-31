98b24a4cef0e15e99c59f162ccb7582421393aab
Bibek <bibek@examsnepal.com>
2025-12-31 16:51:46 +0545

    solution pagination changed

--- src/app/student/solutions/[id]/page.tsx
+++ src/app/student/solutions/[id]/page.tsx
@@ -1,130 +1,117 @@
 'use client';
-import {use, useCallback, useEffect, useState} from "react";
-import examService from "@/services/ExamService/ExamService";
 
+import {use, useCallback, useEffect, useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {useRouter} from "next/navigation";
+
+import examService from "@/services/ExamService/ExamService";
 import {StudentBannerHeader} from "@/components/banner/header";
 import {Button} from "@/components/ui/button";
-import {router} from "next/client";
-import LogoLoading from "@/lib/LogoLoading";
-import {QuestionSolutionCard} from "@/components/Exams/QuestionCard";
+import {QuestionCardSkeleton, QuestionSolutionCard} from "@/components/Exams/QuestionCard";
+import CustomPagination from "@/components/Pagination";
 
 export default function ViewSolution({params}: { params: Promise<{ id: number }> }) {
     const {id} = use(params);
-    const [solution, setSolution] = useState<any>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
     const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
 
-    const fetchSolution = useCallback(async (page: number) => {
-        setLoading(true);
-        setError(null);
-        try {
-            const res = await examService.getExamById({id: Number(id), page});
-            console.log(`Solution Response `, res);
-            setSolution(res?.data?.data || null);
-            setTotalPages(Math.ceil(res?.data?.total / 10));
-        } catch (err) {
-            setError("Failed to fetch solutions data");
-        } finally {
-            setLoading(false);
-        }
+    const fetchSolution = useCallback(async ({queryKey}: any) => {
+        const [_key, examId, page] = queryKey;
+        const res = await examService.getExamById({id: Number(examId), page});
+        return res.data;
+    }, []);
 
-    }, [id]);
+    const {data, isLoading, isError, refetch} = useQuery({
+        queryKey: ["view-solution", id, currentPage],
+        queryFn: fetchSolution,
 
-    useEffect(() => {
-        fetchSolution(currentPage);
-    }, [currentPage, fetchSolution]);
+    });
+
+    const solution = data?.data || [];
+    const totalPages = data?.total ? Math.ceil(data.total / 10) : 0;
 
     useEffect(() => {
         window.scrollTo({top: 0, behavior: "smooth"});
     }, [currentPage]);
 
-    if (loading) return <LogoLoading fullscreen={false}/>;
-    if (error) return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold text-red-500">{error}</h1>
-            <div className={' flex items-center justify-center space-x-2'}>
-                <Button onClick={() => setCurrentPage(1)} variant={'destructive'}
-                        className="mt-4">
-                    Retry
-                </Button>
-                <Button onClick={() => router.push('/student/exams/mock-tests')} className="mt-4 primary-btn">
-                    Go Back
-                </Button>
-            </div>
+    const handlePageChange = useCallback((page: number) => setCurrentPage(page), []);
+
+    if (isLoading) return (
+        <div className="w-full mx-auto my-6 md:my-10 container space-y-2 shadow-md p-3">
+            {Array.from({length: 10}).map((_, index) => (
+                <QuestionCardSkeleton
+                    key={index}/>
+            ))}
         </div>
-    )
+    );
 
-    const handleNext = () => {
-        setCurrentPage((prevPage) => prevPage + 1);
-    };
 
-    const handlePrevious = () => {
-        setCurrentPage((prevPage) => prevPage - 1);
-    };
+    if (isError) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold text-red-500">Failed to fetch solutions data</h1>
+                <div className="flex items-center justify-center space-x-2">
+                    <Button onClick={() => refetch()} variant="destructive" className="mt-4">
+                        Retry
+                    </Button>
+                    <Button onClick={() => router.push('/student/exams/mock-tests')} className="mt-4 primary-btn">
+                        Go Back
+                    </Button>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <section>
-            <div className={'w-full'}>
+            <div className="w-full">
                 <StudentBannerHeader
-                    title={' Sprint Quiz'}
-                    subtitle={'Get instant answers to your questions'}
-                    className={'bg-gradient-to-r from-teal-200 to-teal-400 text-white'}
-                    textClassName={'text-white'}
+                    title="Sprint Quiz"
+                    subtitle="Get instant answers to your questions"
+                    className="bg-gradient-to-r from-teal-200 to-teal-400 text-white"
+                    textClassName="text-white"
                 />
-                <div className={'w-full mx-auto my-6 md:my-10 container space-y-2 shadow-md p-3'}>
-                    <div className={' flex flex-col items-center justify-center'}>
-                        <h1 className={' text-2xl font-bold text-gray-900 mb-2'}>
-                            Total Questions: {solution?.length}
+                <div className="w-full mx-auto my-6 md:my-10 container space-y-2 shadow-md p-3">
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="text-2xl font-bold text-gray-900 mb-2">
+                            Total Questions: {solution.length}
                         </h1>
-                        <h2 className={' text-3xl font-medium text-gray-900 mb-2 font-poppins'}>
-                            <span className={'text-2xl font-semibold'}>Solution</span>
-                            <span className={'text-sm text-gray-500'}> ({totalPages} pages)</span>
+                        <h2 className="text-3xl font-medium text-gray-900 mb-2 font-poppins">
+                            <span className="text-2xl font-semibold">Solution</span>
+                            <span className="text-sm text-gray-500"> ({totalPages} pages)</span>
                         </h2>
                     </div>
-                    {
-                        solution?.map((question: any, index: number) => {
-                                const options = question.options.map((opt: any) => ({
-                                    label: opt.option,
-                                    value: String(opt.id),
-                                    isCorrect: Boolean(opt.value),
-                                }));
-                                const selectedAnswer = question?.user_choosed
-                                    ? String(question.user_choosed)
-                                    : undefined;
-                                return (
-                                    <QuestionSolutionCard key={question.id} questionNumber={(currentPage - 1) * 10 + index + 1}
-                                                  questionText={question?.question}
-                                                  options={options}
-                                                  selectedValue={selectedAnswer}
-                                                  showFeedback={true}
-                                                  correctAnswers={options.filter((opt: any) => opt.isCorrect).map((opt: any) => opt.value)}
-                                                  explanation={question?.explanation}
-                                                  onSelect={function (value: string): void {
-                                                  }}
-                                                  id={question?.id}
-                                    />
-                                )
-
-                            }
-                        )
-                    }
-
-
-                    {/* Navigation & Submit */}
-                    <div className="flex justify-between mt-4">
-                        <Button onClick={handlePrevious} disabled={currentPage === 1} variant="secondary"
-                                className={'border border-gray-300'}>
-                            Previous
-                        </Button>
-                        <Button onClick={handleNext} disabled={currentPage === totalPages} className="primary-btn">
-                            Next
-                        </Button>
-                    </div>
+                    {solution.map((question: any, index: number) => {
+                        const options = question.options.map((opt: any) => ({
+                            label: opt.option,
+                            value: String(opt.id),
+                            isCorrect: Boolean(opt.value),
+                        }));
+                        const selectedAnswer = question?.user_choosed
+                            ? String(question.user_choosed)
+                            : undefined;
+                        return (
+                            <QuestionSolutionCard
+                                key={question.id}
+                                questionNumber={(currentPage - 1) * 10 + index + 1}
+                                questionText={question.question}
+                                options={options}
+                                selectedValue={selectedAnswer}
+                                showFeedback={true}
+                                correctAnswers={options.filter((opt: any) => opt.isCorrect).map((opt: any) => opt.value)}
+                                explanation={question.explanation}
+                                onSelect={() => {
+                                }}
+                                id={question.id}
+                            />
+                        );
+                    })}
+                    <CustomPagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        onPageChangeAction={handlePageChange}
+                    />
                 </div>
-
-
             </div>
         </section>
     );
